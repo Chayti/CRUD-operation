078e69e6e4517f24a6a5afe69633cb192e2c8628
Chayti Saha <chaytisaha98@gmail.com>
2021-10-23 19:25:26 +0600

    delete user

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -3,11 +3,32 @@ import React, { useState, useEffect } from 'react';
 
 const Users = () => {
     const [users, setUsers] = useState([])
+
     useEffect(() => {
         fetch('http://localhost:5000/users')
             .then(res => res.json())
             .then(data => setUsers(data))
     }, [])
+
+    // delete an user
+    const handleDeleteUser = id => {
+        const proceed = window.confirm('Are you sure?')
+        if (proceed) {
+            const url = `http://localhost:5000/users/${id}`
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        alert('deleted successfully')
+                        const remainingUsers = users.filter(user => user._id !== id)
+                        setUsers(remainingUsers)
+                    }
+                })
+        }
+    }
+
     return (
         <div>
             <h2>Users available : {users.length}</h2>
@@ -15,7 +36,7 @@ const Users = () => {
                 users.map(user => <li key={user._id}>
                     {user.name} :: {user.email}
                     <button>Update</button>
-                    <button>Delete </button>
+                    <button onClick={() => handleDeleteUser(user._id)}>Delete </button>
                 </li>)
             }
         </div>
