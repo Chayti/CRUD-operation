e1d13aa5f33a98a3d39d610f736c34f4f7ae3dae
Chayti Saha <chaytisaha98@gmail.com>
2021-10-23 18:55:53 +0600

    add data to database

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+const { MongoClient } = require('mongodb');
 const express = require('express')
+const cors = require('cors');
+
 const app = express()
 const port = process.env.PORT || 5000
-const { MongoClient } = require('mongodb');
+
+// middleware
+app.use(cors());
+app.use(express.json());
 
 // user: mydbuser1
 // pass: tDRBNUDXNkdyXQ4G
@@ -10,43 +16,35 @@ const { MongoClient } = require('mongodb');
 // add one data to database
 const uri = "mongodb+srv://mydbuser1:tDRBNUDXNkdyXQ4G@cluster0.iea9q.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-    const collection = client.db("foodMaster").collection("users");
-    // perform actions on the collection object
-    console.log("hitting db");
 
-    const user = { name: 'Opu Biswas', email: "opu@gmail.com", phone: '0188888888' };
-    collection.insertOne(user)
-        .then(() => {
-            console.log('insert success');
-        })
-
-    // client.close();
-});
-
-// add multiple data to db
 async function run() {
     try {
         await client.connect();
-        const database = client.db("foodMaster");
-        const foods = database.collection("users");
+        const database = client.db('foodMaster');
+        const usersCollection = database.collection('users');
 
-        // create an array of documents to insert
-        const docs = [
-            { name: 'Mamun Prayash', email: "mamun@gmail.com", phone: '0122222222' },
-            { name: 'Niladri Shuvo', email: "shuvo@gmail.com", phone: '0188888888' },
-            { name: 'Tithi Mukharjee', email: "tithi@gmail.com", phone: '015555555' },
-        ];
+        // GET API
+        app.get('/users', async (req, res) => {
+            const cursor = usersCollection.find({})
+            const users = await cursor.toArray()
+            res.send(users)
+        })
 
-        // this option prevents additional documents from being inserted if one fails
-        const options = { ordered: true };
-        const result = await foods.insertMany(docs, options);
-        console.log(`${result.insertedCount} documents were inserted`);
-    } finally {
-        await client.close();
+
+        // POST API
+        app.post('/users', async (req, res) => {
+            const newUser = req.body
+            const result = await usersCollection.insertOne(newUser)
+            console.log('hitting the post', req.body)
+            console.log('added user', result)
+            res.json(result)
+        });
+    }
+    finally {
+        // await client.close();
     }
 }
-run().catch(console.dir);
+run().catch(console.dir)
 
 
 app.get('/', (req, res) => {
